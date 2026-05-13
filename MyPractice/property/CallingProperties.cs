dbebb5e949116ccf0843a7b7b56992016fca4eb2
CharyKandepi <srinivasacharykandepi@gmail.com>
2026-05-13 21:40:59 +0530

    Proprties

---
+++ MyPractice/property/CallingProperties.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPractice.property
+{
+    internal class CallingProperties
+    {
+
+        public static void Main()
+        { 
+           
+           ExampleofProperties exampleofProperties = new ExampleofProperties();
+
+            //exampleofProperties.CollageName = "ANRCollage";      //Set or Write
+
+            //var data=exampleofProperties.CollageName; //get // Read
+
+
+
+            //exampleofProperties.CollageName("ANRCollage",1458);
+
+
+           // exampleofProperties.CollageName = "ANRCollage";
+
+            var data=exampleofProperties.CollageName;
+          Console.WriteLine(data);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MyPractice/property/ExampleofProperties.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPractice.property
+{
+    internal class ExampleofProperties
+    {
+        string _CollageName ="MGRCollage";
+
+
+        public string CollageName
+        {
+            get 
+            { 
+               return _CollageName;
+            }
+
+            set
+            {
+                if (value != null && value != string.Empty)
+                {
+                    _CollageName = value;
+                }
+            }
+
+        }
+
+        //public string CollageName(int pwd)                      //Get  // reading
+        //{     if (pwd==123)
+        //    { 
+        //        return _CollageName;
+
+        //    }
+        //    return null;
+        //}
+
+        //public void CollageName(string CollageName, int? Password = null) //set // Writing
+        //{
+        //    if (Password == 1234)
+        //    {
+        //        _CollageName = CollageName;
+
+        //    }
+        //}
+
+
+        public static void Main()
+        {
+            ExampleofProperties exampleofProperties = new ExampleofProperties();
+
+           // exampleofProperties.CollageName("NTRCollage");
+
+
+
+            Console.WriteLine(exampleofProperties._CollageName);
+
+            Console.ReadLine();
+        }
+
+
+
+    }
+}
